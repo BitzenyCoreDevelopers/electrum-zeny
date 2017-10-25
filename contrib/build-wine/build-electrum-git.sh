f5e363982c56b3b3fe076b6845a50294fdb4c0fe
脇山P <wakiyamap@gmail.com>
2017-10-26 00:08:07 +0900

    change git url

--- contrib/build-wine/build-electrum-git.sh
+++ contrib/build-wine/build-electrum-git.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # You probably need to update only this link
-ELECTRUM_GIT_URL=git://github.com/wakiyamap/electrum-zeny.git
+ELECTRUM_GIT_URL=git://github.com/BitzenyCoreDevelopers/electrum-Zeny.git
 BRANCH=master
 NAME_ROOT=electrum-zeny
 PYTHON_VERSION=3.6.3
